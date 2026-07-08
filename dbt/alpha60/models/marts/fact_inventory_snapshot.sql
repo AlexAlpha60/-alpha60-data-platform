9b155a6071d32e00631357c3ce9c30445b4a8c01
Alex Cleary <YOUR_EMAIL_ADDRESSalex@alpha60.com.au>
2026-07-08 12:02:50 +1000

    Add inventory snapshot warehouse model

---
+++ dbt/alpha60/models/marts/fact_inventory_snapshot.sql
@@ -0,0 +1,36 @@
+SELECT
+    il.inventory_level_key,
+
+    il.location_id,
+    l.location_name,
+    l.city,
+
+    pv.inventory_item_id,
+
+    pv.product_id,
+    pv.variant_id,
+
+    pv.product_title,
+    pv.variant_title,
+
+    pv.sku,
+    pv.vendor,
+    pv.product_type,
+
+    il.available_quantity,
+
+    p.status AS product_status,
+
+    il.inventory_level_updated_at,
+    CURRENT_TIMESTAMP() AS modelled_at
+
+FROM `alpha60-data-platform.stg.shopify_inventory_levels` il
+
+JOIN `alpha60-data-platform.stg.shopify_product_variants` pv
+  ON il.inventory_item_id = pv.inventory_item_id
+
+JOIN `alpha60-data-platform.stg.shopify_products` p
+  ON pv.product_id = p.product_id
+
+JOIN `alpha60-data-platform.stg.shopify_locations` l
+  ON il.location_id = l.location_id
